f57a56fb3507103ff1859f2e07dc44d3412ff5cf
I T_code <oluwagbotemioyeboade@gmail.com>
2023-05-02 23:21:29 +0100

    Adding a react to a website

---
+++ React.js
@@ -0,0 +1,8 @@
+/*The react script tag*/<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+
+ <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+ 
+
+
+/*Jsx file script that make it possible to write 
+Html inside JavaScript*/<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
